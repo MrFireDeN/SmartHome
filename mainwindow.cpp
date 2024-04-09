c1b49020f5d8ed74374fd7e96ff4806132d1b834
Денис Свиридов <36342234+MrFireDeN@users.noreply.github.com>
2024-04-09 13:09:59 +0300

    feat: добавлены комментарии

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,77 +1,87 @@
-
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "QDebug"
 
-
+// Конструктор главного окна
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    ui->setupUi(this); // Инициализация пользовательского интерфейса
 
+    // Настройка различных частей интерфейса
     setupProfile();
     setupHomePage();
     setupSettings();
 }
 
+// Деструктор главного окна
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete ui; // Освобождение ресурсов пользовательского интерфейса
 }
 
+// Настройка профиля пользователя
 void MainWindow::setupProfile() {
-    name = ui->name->text();
+    name = ui->name->text(); // Получение имени пользователя из текстового поля
 
-    // Конекты на кнопки
+    // Настройка поведения при включении/выключении чекбокса "Всегда дома"
     connect(ui->checkBoxAlwaysHome, &QCheckBox::clicked, this, [this](bool checked) {
-        qDebug() << "Always Home: " << checked;
+        qDebug() << "Always Home: " << checked; // Логирование состояния чекбокса
+        // Включение/выключение элементов управления временем работы в зависимости от состояния чекбокса
         ui->timeEditStartWorking->setEnabled(!checked);
         ui->timeEditStopWorking->setEnabled(!checked);
     });
 
+    // Обновление имени пользователя и приветствия при изменении текста в поле ввода имени
     connect(ui->name, &QLineEdit::textChanged, this, [this]() {
-        name = ui->name->text();
-        qDebug() << "name: " << name;
+        name = ui->name->text(); // Обновление имени пользователя
+        qDebug() << "name: " << name; // Логирование нового имени
 
-        changeWelcome();
+        changeWelcome(); // Обновление приветствия
     });
 }
 
+// Настройка домашней страницы
 void MainWindow::setupHomePage() {
-    changeWelcome();
+    changeWelcome(); // Инициализация приветствия на домашней странице
 }
 
-
-
+// Настройка настроек приложения
 void MainWindow::setupSettings() {
-    globalTime = ui->timeEditGlobalTime->time();
+    globalTime = ui->timeEditGlobalTime->time(); // Инициализация глобального времени
 
+    // Настройка поведения при включении/выключении чекбокса "Системное время"
     connect(ui->checkBoxSystemTime, &QCheckBox::clicked, this, [this](bool checked) {
+        // Включение/выключение элементов управления временем и днями недели
         ui->comboBoxWeekday->setEnabled(!checked);
         ui->timeEditGlobalTime->setEnabled(!checked);
 
+        // Установка времени в зависимости от состояния чекбокса
         if (checked) {
-            globalTime = QTime::currentTime();
+            globalTime = QTime::currentTime(); // Текущее системное время
         }
         else {
-            globalTime = ui->timeEditGlobalTime->time();
+            globalTime = ui->timeEditGlobalTime->time(); // Время, выбранное пользователем
         }
 
-        changeWelcome();
+        changeWelcome(); // Обновление приветствия
     });
 
+    // Обновление глобального времени и приветствия при изменении времени
     connect(ui->timeEditGlobalTime, &QTimeEdit::timeChanged, this, [this]() {
-        globalTime = ui->timeEditGlobalTime->time();
+        globalTime = ui->timeEditGlobalTime->time(); // Обновление глобального времени
 
-        changeWelcome();
+        changeWelcome(); // Обновление приветствия
     });
 }
 
+// Метод для обновления текста приветствия в зависимости от времени суток
 void MainWindow::changeWelcome() {
-    QString welcome;
-    int hour = globalTime.hour();
+    QString welcome; // Строка для хранения приветствия
+    int hour = globalTime.hour(); // Получение текущего часа
 
+    // Определение приветствия в зависимости от времени суток
     if (hour >= 4 && hour <= 10) {
         welcome = "Доброе утро, " + name;
     } else if (hour > 10 && hour <= 16) {
@@ -82,6 +92,6 @@ void MainWindow::changeWelcome() {
         welcome = "Доброй ночи, " + name;
     }
 
+    // Установка текста приветствия в пользовательский интерфейс
     ui->labelWelcome->setText(welcome);
 }
-
--- mainwindow.h
+++ mainwindow.h
@@ -1,36 +1,35 @@
-
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include "qdatetime.h"
 #include <QMainWindow>
 
-
-
+// Объявление пространства имен для UI
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+// Определение класса MainWindow, наследующего QMainWindow
 class MainWindow : public QMainWindow
-
 {
-    Q_OBJECT
+    Q_OBJECT // Макрос Q_OBJECT для поддержки мета-объектной системы Qt
 
 public:
+    // Конструктор и деструктор
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
-    void setupProfile();
-    void setupHomePage();
-    void setupSettings();
+    Ui::MainWindow *ui; // Указатель на интерфейс пользователя
+    void setupProfile(); // Метод для настройки профиля пользователя
+    void setupHomePage(); // Метод для настройки домашней страницы
+    void setupSettings(); // Метод для настройки настроек приложения
 
-    void changeWelcome();
+    void changeWelcome(); // Метод для обновления приветствия
 
-    QString name;
-    QTime globalTime;
-    int weekday = 0;
+    QString name; // Имя пользователя
+    QTime globalTime; // Глобальное время, используемое в приложении
+    int weekday = 0; // Текущий день недели (по умолчанию 0)
 };
 
 #endif // MAINWINDOW_H
